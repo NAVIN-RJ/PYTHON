9b0c8e43873ea410fee4c6531569849e7321d010
NAVIN-RJ <90326725+NAVIN-RJ@users.noreply.github.com>
2022-11-17 00:11:35 +0530

    Create operations.py

---
+++ SampleCalculator/operations.py
@@ -0,0 +1,18 @@
+
+def addition(first, second):
+    return first + second
+
+def subtraction(first, second):
+    return first - second
+
+def product(first, second):
+    return first * second
+
+def floatDivision(first, second):
+    return first / second # 5/2 = 2
+
+def integerDivision(first, second):
+    return first // second # 5//2 = 2
+
+def modulo(first, second):
+    return first % second
